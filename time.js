648c80ee759777ed8f00cc9b63ea85ee9f2616f7
Lvpike <949715925@qq.com>
2022-09-24 00:07:29 +0800

    Add files via upload

---
+++ time.js
@@ -0,0 +1,72 @@
+/*var i = 0;
+var k = 0;
+
+var time1;
+*/
+
+var j = 30; //休息完毕的时间，单位分钟
+var g = 1; //该休息了的时间，单位分钟
+var worker1;
+function timeToRest() {
+  // var i=1;
+  // var j = document.getElementById("button1").getAttribute("value");
+  // for (let i = 0; i < 3; i++) {
+  //   //   setTimeout(() => {
+  //   //      alert(i)
+  //   //   }, 5000 * i)
+  //   console.log(i);
+  //  };
+  /*document.getElementById("button1").disabled = true;
+  time1 = setTimeout(() => {
+    if (k < j) {
+      i++;
+      // console.log(i);
+      if (i === 60) {
+        i = 0;
+        k = k + 1;
+      }
+      // console.log(k);
+      document.getElementById("p2").innerHTML = k + ":" + i;
+      timeToRest();
+    } else {
+      i = 0;
+      k = 0;
+      music.play();
+      timeToRest();
+    }
+
+  }, 1000);*/
+  // setTimeout(() => {
+  //   music.play();
+  //   alert("注意休息，现在开始休息十分钟");
+  //   console.log(i);
+  //   timeToRest();
+  // }, j);
+  worker1 = new Worker("worker.js");
+  worker1.onmessage = (e) => {
+    console.log(e.data);
+    if (e.data[0] !== undefined) {
+      document.getElementById("p2").innerHTML = e.data[0] + ":" + e.data[1];
+    }
+    if (e.data[0] === 0 && e.data[1] === 3) {
+      console.log("播放音乐");
+      // worker1.postMessage(music);
+    }
+  };
+}
+
+function timeToStop() {
+  worker1.terminate();
+  document.getElementById("button1").disabled = false;
+  // clearTimeout(time1);
+  i = 0;
+  k = 0;
+  document.getElementById("p2").innerHTML = k + ":" + i;
+}
+// function setTime(i){
+//     setTimeout(console.log(i),4000);
+// }
+// function myFun(){
+//     var obj =document.getElementById("p1");
+//     obj.innerHTML='开始放松';
+// }
---
+++ worker.js
@@ -0,0 +1,58 @@
+var i = 0;
+var k = 0;
+var j = 30; //休息完毕的时间，单位分钟
+var g = 1; //该休息了的时间，单位分钟
+var time1;
+var message = [];
+// self.addEventListener(
+//   "message",
+//   function (e) {
+//     console.log(e);
+//   },
+//   false
+// );
+function timeToWork() {
+  // var i=1;
+  // var j = document.getElementById("button1").getAttribute("value");
+  // for (let i = 0; i < 3; i++) {
+  //   //   setTimeout(() => {
+  //   //      alert(i)
+  //   //   }, 5000 * i)
+  //   console.log(i);
+  //  };Work
+  //   document.getElementById("button1").disabled = true;
+  time1 = setTimeout(() => {
+    if (k < j) {
+      i++;
+      // console.log(i);
+      if (i === 60) {
+        i = 0;
+        k = k + 1;
+      }
+      // console.log(k);
+      // document.getElementById("p2").innerHTML = k + ":" + i;
+      message[0] = k;
+      message[1] = i;
+      timeToWork();
+    } else {
+      i = 0;
+      k = 0;
+      music.play();
+      message[0] = k;
+      message[1] = i;
+      timeToWork();
+    }
+    if (k === g && i === 0) {
+      music.play();
+    }
+  }, 1000);
+
+  // setTimeout(() => {
+  //   music.play();
+  //   alert("注意休息，现在开始休息十分钟");
+  //   console.log(i);
+  //   timeToRest();
+  // }, j);
+  self.postMessage(message);
+}
+timeToWork();
